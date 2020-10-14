41a858968491fb5ee63e06211bd57b123566e99d
chul0129 <yk2915@naver.com>
2020-10-14 21:51:51 +0900

    수정

--- 20161359 게임만들기.cpp
+++ 20161359 게임만들기.cpp
@@ -1,6 +1,6 @@
 ﻿#include<windows.h>
 #include <iostream>
-#include <bangtal.h>
+#include <bangtal>
 #include <ctime>
 
 using namespace bangtal;
@@ -9,10 +9,12 @@ using namespace std;
 int main()
 {
 	auto scene = Scene::create("피해서 지나가기 Game", "gimage/장면.jpg");
-	int dogx = 300, dogy = 500;
+	int dogx = 0, dogy = 400;
 	auto dog = Object::create("gimage/강아지.jpg", scene, dogx, dogy);
 	auto cloud = Object::create("gimage/사탕.jpg", scene, 600, 500);
-	auto candy = Object::create("gimage/번개.png", scene, 100, 500);
+	auto candy = Object::create("gimage/번개.png", scene, 200, 0);
+	auto squ = Object::create("gimage/다람쥐.jpg", scene, 900, 0);
+	auto rab = Object::create("gimage/토끼.jpg", scene, 600, 0);
 	int cnt = 0;
 	auto timer = Timer::create(0.02f);
 	auto count = 0;
@@ -21,18 +23,29 @@ int main()
 	timer->setOnTimerCallback([&](TimerPtr timer)->bool {  //타이머 호출 게임 시작
 			dogx+=5;
 			dogy-=5;
-			cnt++;
 			dog->locate(scene, dogx, dogy);
 			dog->setOnMouseCallback([&](ObjectPtr object,int x, int y, MouseAction action)->bool {
-				dogy += 35;
+				dogy += 50;
 				dog->locate(scene, dogx, dogy);
+				
 				return true;
 				});
-		count++;
-		if (count < 1000) {
-			timer->set(0.02f);
-			timer->start();
-		}
+
+		
+			if (dogx < 1130 && dogy>0) {
+				timer->set(0.02f);
+				timer->start();
+			}
+			if (dogx >= 1130)
+			{
+				dogx = 0;
+				timer->start();
+			}
+			if (dogy <= 0)
+			{
+				showMessage("게임 종료");
+			}
+
 		return true;
 		});
 
